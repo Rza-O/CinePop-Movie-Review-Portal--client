425f3238e448f5fa4074f20f70fe32efa030d51d
appoclayptor <shahreza1414@gmail.com>
2024-12-05 03:51:35 +0600

    added all the validations for the inputs

--- src/Pages/AddMovies.jsx
+++ src/Pages/AddMovies.jsx
@@ -11,19 +11,21 @@ const animatedComponents = makeAnimated();
 
 const AddMovies = () => {
     // Context to get the user
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     console.log(user?.email);
     // React form hook
-    const { register, handleSubmit, watch, setValue } = useForm();
+    const { register, handleSubmit, setValue, formState: {errors} } = useForm();
     // Rating state
     const [rating, setRating] = useState(0);
+    // error state
+    const [error, setError] = useState('');
     // rating change handler
     const handleRatingChange = (rate) => {
         setRating(rate)
     }
     // generating year programmatically 
     const currentYear = new Date().getFullYear();
-    const years = Array.from({length: currentYear - 1971 + 1}, (_, index)=> currentYear - index)
+    const years = Array.from({ length: currentYear - 1971 + 1 }, (_, index) => currentYear - index)
 
     // for multiple genres selection
     const options = [
@@ -39,6 +41,11 @@ const AddMovies = () => {
 
     // Add movies form submission with react form hook
     const handleAddMovies = (data) => {
+        setError('')
+        if(rating === 0){
+            setError('Movie has to be rated')
+            return
+        }
         const movieData = { ...data, rating, duration: parseInt(data.duration), year: parseInt(data.year) }
         console.log(movieData);
     }
@@ -46,6 +53,8 @@ const AddMovies = () => {
 
     return (
         <div>
+
+
 
             <div className='text-center space-y-2 my-5'>
                 <h1 className='text-5xl font-bold'>Recently watched a movie?</h1>
@@ -56,32 +65,55 @@ const AddMovies = () => {
                     <img src={reviewImg} alt="" className='w-3/4 mx-auto'/>
                 </div> */}
                 <div className="bg-base-100 w-11/12 mx-auto">
+                {/* form Starts here */}
                     <form className="card-body" onSubmit={handleSubmit(handleAddMovies)}>
-
                         <div className='flex gap-2'>
+                            {/* Title input */}
                             <div className="form-control w-1/2">
                                 <label className="label ">
                                     <span className="label-text font-bold">Movie Title</span>
                                 </label>
-                                <input {...register('title')} type="text" placeholder="title" className="input input-bordered" required />
+                                <input {...register('title', {
+                                    validate: {
+                                        minLength: (value) =>
+                                            value.length >= 2 || "Movie title at least has to be 2 character long"
+                                    }
+                                })} type="text" placeholder="title" className="input input-bordered" required />
+                                {errors?.title && <p className='text-xs mt-1 text-secondary'>{errors.title.message}</p>}
                             </div>
+                            {/* Poster Url */}
                             <div className="form-control w-1/2">
                                 <label className="label">
                                     <span className="label-text font-bold">Poster URL</span>
                                 </label>
-                                <input {...register('poster')} type="text" placeholder="poster" className="input input-bordered" required />
+                                <input {...register('poster', {
+                                    validate: {
+                                        isLink: (value) =>
+                                            /^(https?:\/\/)?([\w\-]+(\.[\w\-]+)+)([\/\w\-?.=&]*)*$/.test(value) || "Poster has to be a valid URL"
+                                    }
+                                })} type="text" placeholder="poster" className="input input-bordered" required />
+                                {errors?.poster && <p className='text-xs mt-1 text-secondary'>{errors.poster.message}</p>}
                             </div>
                         </div>
 
 
 
                         <div className='flex gap-2'>
+                            {/* runtime input */}
                             <div className="form-control w-1/2">
                                 <label className="label">
                                     <span className="label-text font-bold">Runtime</span>
                                 </label>
-                                <input {...register('duration')} type="number" placeholder="duration" className="input input-bordered [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" required />
+                                <input {...register('duration', {
+                                    validate: {
+                                        minLength: (value) =>
+                                            value >= 60 || "Film duration has to be greater than 60 mins"
+                                    }
+                                })} type="number" placeholder="duration" className="input input-bordered [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" required />
+                                {errors.duration && <p className='text-xs mt-1 text-secondary'>{errors.duration.message}</p>}
                             </div>
+
+                            
                             <div className="form-control w-1/2">
                                 <label className="label">
                                     <span className="label-text font-bold">Release Year</span>
@@ -110,19 +142,20 @@ const AddMovies = () => {
                                 placeholder='Select Genres'
                                 options={options}
                                 isMulti
+                                required
                                 onChange={(selectedOptions) => {
                                     setValue('genres', selectedOptions ? selectedOptions.map((option) => option.value) : [])
                                 }}
                             />
                         </div>
 
-                        
+
 
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Summary</span>
                             </label>
-                            <textarea {...register('summary')} className="textarea textarea-bordered" placeholder="Write a brief summary here"></textarea>
+                            <textarea {...register('summary')} className="textarea textarea-bordered" placeholder="Write a brief summary here" required></textarea>
                         </div>
 
 
@@ -139,9 +172,17 @@ const AddMovies = () => {
                                     fillColor="#72163E"
                                     emptyColor="gray"
                                     transition
+                                    
                                 />
                                 <span>{rating} / 5</span>
                             </div>
+                            {
+                                error && (
+                                    <label className="label text-sm text-secondary">
+                                        {error}
+                                    </label>
+                                )
+                            }
                         </div>
 
                         <div className="form-control mt-6">
