38c5631f56b6b7e0a2e78dfda49a75cefdd55450
appoclayptor <shahreza1414@gmail.com>
2024-12-06 01:07:24 +0600

    added all movies route

--- src/Pages/AllMovies.jsx
+++ src/Pages/AllMovies.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import MovieCard from '../Components/MovieCard';
 
 const AllMovies = () => {
+    const movieData = useLoaderData()
+    console.log(movieData);
     return (
         <div>
-            This is all movies page
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4'>
+                {
+                    movieData.map((movie) => <MovieCard movie={movie}></MovieCard>)
+                }
+            </div>
         </div>
     );
 };
