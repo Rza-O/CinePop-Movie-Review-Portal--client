5d732e964765ae46b29ccf2cab60ffc9c3ed037d
appoclayptor <shahreza1414@gmail.com>
2024-12-07 17:04:31 +0600

    implemented search option for all movies section

--- src/Pages/AllMovies.jsx
+++ src/Pages/AllMovies.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import MovieCard from '../Components/MovieCard';
 
 const AllMovies = () => {
-    const movieData = useLoaderData()
+    const data = useLoaderData()
+
+    const [search, setSearch] = useState('');
+
+    const [movieData, setMovieData] = useState(data);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/movies?search=${search}`)
+            .then(res => res.json())
+            .then(data => {
+                setMovieData(data);
+            })
+    }, [search]);
+
+
+
     console.log(movieData);
     return (
         <div className='w-11/12 mx-auto'>
             <h2 className='mt-6 text-center text-4xl font-bold'>Our Movies Collection</h2>
+            <div className='flex justify-center my-5'>
+                <input
+                onChange={(e)=> setSearch(e.target.value)}
+                    type="text"
+                    name='search'
+                    placeholder="🔍 Search movies"
+                    className="input input-bordered border-secondary w-full max-w-lg" />
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4'>
                 {
                     movieData.map((movie) => <MovieCard movie={movie}></MovieCard>)
